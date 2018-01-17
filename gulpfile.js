8c79c4a1f8cd5994e6a8ab13136f8704f7433ad6
Mido <midorin6497@gmail.com>
2018-01-17 16:55:20 +0800

    修改gulp匯出css路徑

--- gulpfile.js
+++ gulpfile.js
@@ -3,13 +3,14 @@ var gulp = require('gulp');
 var sass = require('gulp-sass');
 
 gulp.task('sass', function () {
-    gulp.src('css/*.scss')
+    gulp.src('scss/*.scss')
         .pipe(sass())
-        .pipe(gulp.dest(function (f) {
-            return f.base;
-        }))
+        // .pipe(gulp.dest(function (f) {
+        //     return f.base;
+        // }))
+        .pipe(gulp.dest('css')); // sass 編譯完成後的匯出資料夾
 });
 
 gulp.task('default', ['sass'], function () {
-    gulp.watch('css/*.scss', ['sass']);
+    gulp.watch('scss/*.scss', ['sass']);
 })
